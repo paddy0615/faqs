15e56d0184d5cec978ffd70ced95ecd5bde9cbf6
Paddy <942539456@qq.com>
2018-10-29 10:48:05 +0800

    [客户前端]--添加online chat功能,和样式修改

--- demo/src/main/resources/static/js/index.js
+++ demo/src/main/resources/static/js/index.js
@@ -1,4 +1,116 @@
 var myapp = angular.module("myapp",[]);
+// online chat
+function onlineChat(langId) {
+    if(langId == 1){
+        // 香港 (繁體)
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/8168ca61-a381-422e-9803-44e04dabc878.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("聯絡我們!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("透過線上客服與我們聯絡"); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }else if(langId == 2){
+        // 中国 简体
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/cd66238e-805c-4fd2-b3e1-ccabddaf8a12.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("联络我们!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("透过在线客服与我们联络"); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }else if(langId == 3){
+        // 台灣 (繁體)
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/8168ca61-a381-422e-9803-44e04dabc878.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("聯絡我們!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("透過線上客服與我們聯絡"); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }else if(langId == 4){
+        // 日本 (日本語))
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/d1fa9911-1e41-4df1-a090-a1fd9454a030.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("連絡先情報!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("私たちとチャットしましょう"); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }else if(langId == 5){
+        // 대한민국(한국어)
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/ed84ba43-860a-43fc-87ac-29e1fb8ce6ab.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("연락처!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("연락해 주십시오."); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }else{
+        // Hong Kong (EN)
+        var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+        se.src = 'https://storage.googleapis.com/sonic-teleservices/js/ff4b2b7a-dfbb-47d8-9e51-766c426eb3cb.js';
+        var done = false;
+        se.onload = se.onreadystatechange = function() {
+            if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+                done = true;
+                SonicTeleservices.setTitle("Contact us!"); // for the pre-chat form
+                SonicTeleservicesChat.setTitle("Chat with us!"); // for the chat-form
+                SonicTeleservices.setProactiveAutocloseDelay(0.5);// delay proactive auto close after 30 sec
+            }
+        };
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+    }
+}
+// www.hkexpress.com
+function getHKE(langId){
+    var suffix = "";
+    if(langId == 1){
+        // 香港 (繁體)
+        suffix = "zh-hk";
+    }else if(langId == 2){
+        // 中国 简体
+        suffix = "zh-cn";
+    }else if(langId == 3){
+        // 台灣 (繁體)
+        suffix = "zh-tw";
+    }else if(langId == 4){
+        // 日本 (日本語))
+        suffix = "ja";
+    }else if(langId == 5){
+        // 대한민국(한국어)
+        suffix = "ko";
+    }else if(langId == 6){
+        // Hong Kong (EN)
+        suffix = "en-hk";
+    }
+    clicked("https://www.hkexpress.com/"+suffix);
+}
 
 // index
 myapp.controller("indexController",["$scope","$http",function ($scope, $http) {
@@ -13,6 +125,7 @@ myapp.controller("indexController",["$scope","$http",function ($scope, $http) {
     $scope.searchShow = false;
     // 初始化
     into($scope.langId,$scope.catId);
+    onlineChat($scope.langId);
     function into(langID,catId){
         $http({
             method : 'post',
@@ -48,7 +161,6 @@ myapp.controller("indexController",["$scope","$http",function ($scope, $http) {
             }
         })
     }
-
     // 语言事件
     $scope.clickLanguage = function() {
         if($scope.isGetUrl){
@@ -61,7 +173,7 @@ myapp.controller("indexController",["$scope","$http",function ($scope, $http) {
     // 类别事件,idnex当前下标，id:类别ID
     $scope.clickCategory = function (idnex,cat) {
         if(idnex==0){
-            clicked("https://www.hkexpress.com/");
+            getHKE($scope.langId);
         }else{
             if($scope.isGetUrl){
                 var url = "/faqs/index?langId="+cat.langId+"&catId="+cat.id;
@@ -95,25 +207,10 @@ myapp.controller("indexController",["$scope","$http",function ($scope, $http) {
     $scope.onKeyup = function(event){
        // $scope.arr1=$filter("filter")(arr,document.getElementById("wei").value);
         var e = event || window.event || arguments.callee.caller.arguments[0];
-       /* if(e && e.keyCode==27){ // 按 Esc
-            //要做的事情
-            getSearch(s);
-        }
-        if(e && e.keyCode==113){ // 按 F2
-            //要做的事情
-            getSearch(s);
-        }
-        if(e && e.keyCode==13){ // enter 键
-            getSearch(s);
-        }*/
         $scope.getSearch();
     }
 
 }]);
-
-
-
-
 
 // indexDetailed
 myapp.controller("indexDetailedController",["$scope","$http","$sce",function ($scope, $http, $sce) {
@@ -137,6 +234,7 @@ myapp.controller("indexDetailedController",["$scope","$http","$sce",function ($s
                 /* 成功*/
                 $scope.result = data.result;
                 $scope.langId = data.result.langId;
+                onlineChat($scope.langId);
                 $scope.detailed = data.result.detailed;
                 // 显示内容
                 $scope.content = $sce.trustAsHtml($scope.detailed.content);
@@ -159,7 +257,7 @@ myapp.controller("indexDetailedController",["$scope","$http","$sce",function ($s
     // 类别事件,idnex当前下标，id:类别ID
     $scope.clickCategory = function (idnex,cat) {
         if(idnex==0){
-            clicked("https://www.hkexpress.com/");
+            getHKE($scope.langId);
         }else{
             if($scope.isGetUrl){
                 var url = "/faqs/index?langId="+cat.langId+"&catId="+cat.id;
